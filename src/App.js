0fcd9a2e9597ec669bdd6992f5148d981b63f9e2
윤석 한 <hys6078@naver.com>
2021-02-01 16:38:38 +0900

    add video background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Nav from './Nav';
+import Main from './Main';
+import Home from './Home';
+import Skin from './Skin';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <section>
+      <Router>
+        <Nav/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+        </Switch>
+      </Router>
+    </section>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import Tube from './Tube';
+
+function Main(){
+    return(
+        <div>
+        <div className="video">
+            <Tube videoId='lXWSn8lAn2k'/>
+        </div>
+        <h1>sdafsf</h1>
+        </div>
+    )
+}
+
+export default Main;
