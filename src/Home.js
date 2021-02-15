a4b5f48255a77852518b65e558294ec10a3dfeb8
윤석 한 <hys6078@naver.com>
2021-02-15 14:42:04 +0900

    add swiper

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,24 @@ import HeroSection from './HeroSection'
 import Cards from './Cards'
 import Footer from './Footer'
 import Scroll from './Scroll'
+import Slider from './Slider'
 import './style.css'
 import {
     ScrollingProvider,
     useScrollSection,
     Section,
   } from 'react-scroll-section';
-  const StaticMenu = () => {
+
+const StaticMenu = () => {
     const homeSection = useScrollSection('home');
     const aboutSection = useScrollSection('about');
-   
+   function aa(){
+    homeSection.onClick(true);
+    homeSection.selected=true;
+   }
     return (
       <ul>
-        <li onClick={homeSection.onClick} selected={homeSection.selected}>
+        <li onClick={aa}>
           Home
         </li>
         <li onClick={aboutSection.onClick} selected={aboutSection.selected}>
@@ -22,27 +27,19 @@ import {
         </li>
       </ul>
     );
-  };
-  function Home(){
+};
+function Home(){
     return(
         <ScrollingProvider>
             <StaticMenu/>
             <HeroSection />
             <Section id="home"><Cards /></Section>
             <Section id="about"><Footer/></Section>
+            <Slider/>
             <Scroll/>
         </ScrollingProvider>
     );
 }
-// function Home(){
-//     return(
-//         <>
-//             <HeroSection />
-//             <Cards />
-//             <Footer/>
-//             <Scroll/>
-//         </>
-//     );
-// }
+
 
 export default Home;
---
+++ src/Slider.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {Navigation, Pagination} from 'swiper';
+import 'swiper/swiper-bundle.css'
+
+// Import Swiper styles
+SwiperCore.use([Navigation, Pagination]);
+export default function SimpleSlider() {
+  // var settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   cssEase:"linear",
+  //   arrows:true,
+  //   className:"slides"
+  // };
+  return (
+      <div style={{backgroundColor:'white'}}>
+
+      
+    {/* <Slider {...settings}>
+      <div className="slider">
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider> */}
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      // pagination={{ clickable: true }}
+      // scrollbar={{ draggable: true }}
+      draggable={false}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>
+        <img src="https://placeimg.com/40/40/1"/>
+      </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+    </div>
+  );
+}
